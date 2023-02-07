033a89821dd203c6631f07fb8fe1f6a59656cda2
william rodrigues <38387964+williamrx777@users.noreply.github.com>
2023-02-07 16:38:16 -0300

    first commit

---
+++ divulgar/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Tag,Raca,Pet
+
+# Register your models here.
+
+admin.site.register(Tag),
+admin.site.register(Raca),
+admin.site.register(Pet),
+
